53606b234c161425e36787a3c0dd73869f45d9a9
Sergey.Vilkov <1999seregavilkov@gmail.com>
2024-08-17 19:04:40 +0500

    Update City.cs

--- EmployeePortal.Web/Models/Entities/City.cs
+++ EmployeePortal.Web/Models/Entities/City.cs
@@ -6,7 +6,7 @@ namespace EmployeePortal.Web.Models.Entities
 	{
 		public int? ID { get; set; }
 
-        public string? Name { get; set; }
+        	public string? Name { get; set; }
 
 		public List<Employee>? Employees { get; set; } 
 	}
